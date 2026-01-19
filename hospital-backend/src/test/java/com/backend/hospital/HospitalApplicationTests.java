294d114e35ba1343ae94045ade0bb62e40326a77
XhoXhoT <xhonitinaj@yahoo.com>
2026-01-19 19:57:27 +0100

    Test

---
+++ hospital-backend/src/test/java/com/backend/hospital/HospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
