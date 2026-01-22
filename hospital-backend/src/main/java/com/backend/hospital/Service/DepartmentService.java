ddf1d01f6a80dac0f200654cab96d12f4c3cb634
XhoXhoT <xhonitinaj@yahoo.com>
2026-01-22 21:50:56 +0100

    Define demo service logic

---
+++ hospital-backend/src/main/java/com/backend/hospital/Service/DepartmentService.java
@@ -0,0 +1,43 @@
+package com.backend.hospital.Service;
+
+import java.util.*;
+
+import com.backend.hospital.Entity.Department;
+import com.backend.hospital.Enums.BedStatus;
+import com.backend.hospital.Exceptions.ResourceNotFoundException;
+import com.backend.hospital.Repository.BedRepository;
+import com.backend.hospital.Repository.DepartmentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+    private BedRepository bedRepository;
+
+
+    public List<Department> getAllDepartments() {
+        return departmentRepository.findAll();
+    }
+
+    public Department getDepartmentById(Long id) {
+        return departmentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Department not found with id: " + id));
+    }
+
+    // Admin: create department
+    public Department createDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+
+    public long getFreeBeds(Long departmentId) {
+        return bedRepository.countByRoom_Department_IdAndStatus(
+                departmentId,
+                BedStatus.FREE
+        );
+    }
+
+
+}
